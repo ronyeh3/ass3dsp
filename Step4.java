13ee218d0c4f15dbe70ea08d40bce677053e1710
tamireyal <36245244+tamireyal@users.noreply.github.com>
2018-03-04 22:46:40 +0200

    step4 local

--- Step4.java
+++ Step4.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -13,6 +16,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -30,11 +34,14 @@ public class Step4 {
 	static HashSet<String> hfw = new HashSet<String>();  // onegram!!!!!!!!!!
 	static HashSet<String> hooks = new HashSet<String>();
 	static HashSet<String> notcws = new HashSet<String>(); // there are less not CWs than CWs, working with the "not" for efficiency
+	// because we need them just to make sure we don't choose target words that are very common (is, the ...)
 
 
 	public static class MapperClass extends Mapper<LongWritable, Text, Text, Text> {
 		private static final String type1 = "-1-";
 		private static final String type2 = "-2-";
+		
+		
 		protected void setup(Context context) throws IOException, InterruptedException {
 
 			Configuration conf = context.getConfiguration();
@@ -51,18 +58,28 @@ public class Step4 {
 				}
 			}
 			else {
-				System.err.println("[Mapper step5]  Error cacheFile is Empty!!! ");
+				System.err.println("[Mapper step5]  ERROR: cacheFile is Empty!!! ");
 			}
-			System.out.println("[step5 mapper] Finished loading cache Files.");
+			System.out.println("[Mapper Step5] INFO: Finished loading N1 File.");
+
 
 		}
-		public void readFile(FSDataInputStream in, String filename) throws IOException {
+		public void readFile(InputStream in, String filename) throws IOException {
+
+			System.out.println("reading a file ...");
+
+			BufferedReader joinReader = new BufferedReader(new InputStreamReader(in, "UTF8"));
+
 			HashSet<String> currList;
 			if (filename.substring(0, 3).equals("hfw"))
 				currList = hfw;
-			else
+			else if (filename.substring(0, 3).equals("hoo")) //hook
 				currList = hooks;
-			BufferedReader joinReader = new BufferedReader(new InputStreamReader(in, "UTF8"));
+			else if (filename.substring(0, 3).equals("not"))  //not-content-word
+				currList = notcws;
+			else
+				return;
+
 			String line;
 			while ((line = joinReader.readLine()) != null) {
 				String keyValue = line.toString();
@@ -70,27 +87,37 @@ public class Step4 {
 			}
 
 		}
-
 		public void map(LongWritable LongWritable, Text value, Context context) throws IOException,  InterruptedException {
-			String ngram = value.toString();
-
+			String ngram = value.toString().split("\\t")[0];
 			String[] ngramWords = ngram.split("\\s+");
 			String target, pattern, hookword;
+			System.out.println("Current ngram: "+ngram);
 			if (hfw.contains(ngramWords[0]) && hfw.contains(ngramWords[2]) && hfw.contains(ngramWords[4])){
-				if (hooks.contains(ngramWords[1])) {   //[3]<fc
-					hookword = ngramWords[1];
-					target = ngramWords[3];  // not is - contain 
-					pattern = ngramWords[0] + " " + ngramWords[2] + " " + ngramWords[4];
-					context.write(new Text(type1+"\t"+hookword), new Text(pattern+"##"+target));
-					context.write(new Text(type2+"\t"+pattern), new Text(hookword));
+				if (hooks.contains(ngramWords[1])) {  
+					if (!notcws.contains(ngramWords[3])) {// //[3]<fc - so it is content word
+						System.out.println("1EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT!\n"
+								+"EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT!\n"+
+								"EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT!\n"
+								+ ngram);
+						hookword = ngramWords[1];
+						target = ngramWords[3];  // not is - contain 
+						pattern = ngramWords[0] + " " + ngramWords[2] + " " + ngramWords[4];
+						context.write(new Text(type1+"\t"+hookword), new Text(pattern+"##"+target));
+						context.write(new Text(type2+"\t"+pattern), new Text(hookword));
+					}
 				}
 				else if (hooks.contains(ngramWords[3])) {
-					hookword = ngramWords[3];
-					target = ngramWords[1];
-					pattern = ngramWords[0] + " " + ngramWords[2] + " " + ngramWords[4];
-					context.write(new Text(type1+"\t"+hookword), new Text(pattern+"##"+target));
-					context.write(new Text(type2+"\t"+pattern), new Text(hookword));
-
+					if (!notcws.contains(ngramWords[1])) {
+						System.out.println("2EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT!\n"
+								+"EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT!\n"+
+								"EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT! EMIT!\n"
+								+ ngram);
+						hookword = ngramWords[3];
+						target = ngramWords[1];
+						pattern = ngramWords[0] + " " + ngramWords[2] + " " + ngramWords[4];
+						context.write(new Text(type1+"\t"+hookword), new Text(pattern+"##"+target));
+						context.write(new Text(type2+"\t"+pattern), new Text(hookword));
+					}
 				}
 
 			}  
@@ -99,7 +126,7 @@ public class Step4 {
 
 	public static class ReducerClass extends Reducer<Text,Text,Text,Text> {
 		private MultipleOutputs<Text,Text> mos;
-		
+
 		public void setup(Context context) {
 			mos = new MultipleOutputs<Text,Text>(context);
 		}
@@ -113,7 +140,7 @@ public class Step4 {
 				}
 				patternsAndTargets = patternsAndTargets.substring(0,patternsAndTargets.length());
 				mos.write("byHook", new Text(actualKey), new Text(patternsAndTargets));
-				mos.write("byHook", new Text(""), null);
+				//				mos.write("byHook", new Text(""), null);
 			}
 			else { // if (key.toString().charAt(1) == '2')
 				String hookWords="";
@@ -123,7 +150,7 @@ public class Step4 {
 				}
 				hookWords = hookWords.substring(0,hookWords.length());
 				mos.write("byPattern", new Text(actualKey), new Text(hookWords));
-				mos.write("byPattern", new Text(""), null);
+				//				mos.write("byPattern", new Text(""), null);
 			}
 		}
 		public void cleanup(Context context) throws IOException {
@@ -136,17 +163,17 @@ public class Step4 {
 
 
 	}
-	
+
 
 
 	public static void main(String[] args) throws Exception {
-		
+
 		System.load("C:/Users/Tamir/Desktop/lzo2.dll");
 		System.setProperty("hadoop.home.dir", "C:/hadoop-2.6.2");
-		
-//		System.load("C:/Users/RONlptp/eclipse-workspace/ass2localRunner/lib/lzo2.dll");
-//		System.setProperty("hadoop.home.dir", "E:\\hadoop-2.6.2");
-		
+
+		//		System.load("C:/Users/RONlptp/eclipse-workspace/ass2localRunner/lib/lzo2.dll");
+		//		System.setProperty("hadoop.home.dir", "E:\\hadoop-2.6.2");
+
 		Configuration conf = new Configuration();
 		Job job = new Job(conf);
 		job.setJarByClass(Step4.class);
@@ -169,6 +196,7 @@ public class Step4 {
 		FileStatus[] list = fs.globStatus(hfwAndHooks);  //TODO s3 is object 
 		for (FileStatus status : list) {
 			job.addCacheFile(status.getPath().toUri());
+			System.out.println("--CACHE: Added: "+status.getPath().toUri().toString());
 		}
 		FileOutputFormat.setOutputPath(job, new Path(args[2]));		  
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
