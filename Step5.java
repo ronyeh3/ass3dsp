36663c6f63b38b0bb57c9452cc4bc59b6c53211d
ron yehuda <ronyeh@post.bgu.ac.il>
2018-03-06 16:23:23 +0200

    step 5 good

--- Step5.java
+++ Step5.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -36,80 +38,96 @@ import com.google.gson.reflect.TypeToken;
 import org.apache.hadoop.io.LongWritable;
 
 
+/* input - step 4 by hooks 
+ * 
+ */
+
 public class Step5 {
 
 	private final static float S = (float) 2/3;
 	public static void main(String[] args) throws Exception {
 
-		FileInputStream fstream = new FileInputStream(args[0]); /// make this map-red
-		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-		HashMap<String,HashMap<String, List<String>>> hooksAndClusters = new HashMap<String, HashMap<String,List<String>>>();
-		String strLine;
-		String[] splittedLine;
-		String[] splittedPatternsTargets;
-		String[] currPatternAndTarget;
-		String hookWord, patternsAndTargets;
-		String target, pattern;
-		Gson gson = new Gson();
+		FilenameFilter filter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("byHook");
+			}};
+			File folder = new File(args[0]);
+			File[] listOfFiles = folder.listFiles(filter);
 
-		
-		///first - read by-paterns and group togheter
-		while ((strLine = br.readLine()) != null) {   
-			if (strLine.length() < 2)
-				continue;
-			splittedLine = strLine.split("\\t");
-			hookWord = splittedLine[0];
-			hooksAndClusters.put(hookWord, new HashMap<String,List<String>>());
-			HashMap<String, List<String>> currHookHashMap = hooksAndClusters.get(hookWord);// cluster is {target,[array of patterns]}    
-//for each hook , cluster patternt by target
-			patternsAndTargets = splittedLine[1];
-			splittedPatternsTargets = patternsAndTargets.split("\\|");
-			for (String pair : splittedPatternsTargets) {
-				currPatternAndTarget = pair.split("##");
-				pattern = currPatternAndTarget[0];
-				target = currPatternAndTarget[1];
-				if (currHookHashMap.containsKey(target)) {
-					if (!currHookHashMap.get(target).contains(pattern))
+			HashMap<String,HashMap<String, List<String>>> hooksAndClusters = new HashMap<String, HashMap<String,List<String>>>();
+			String strLine;
+			String[] splittedLine;
+			String[] splittedPatternsTargets;
+			String[] currPatternAndTarget;
+			String hookWord, patternsAndTargets;
+			String target, pattern;
+			Gson gson = new Gson();
+
+			for (int i = 0; i < listOfFiles.length; i++) {
+				File file = listOfFiles[i];
+
+				FileInputStream fstream = new FileInputStream(file); /// make this map-red
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+				///first - read by-paterns and group togheter
+				while ((strLine = br.readLine()) != null) {   
+					if (strLine.length() < 2)
+						continue;
+					splittedLine = strLine.split("\t");
+					hookWord = splittedLine[0];
+					hooksAndClusters.put(hookWord, new HashMap<String,List<String>>());
+					HashMap<String, List<String>> currHookHashMap = hooksAndClusters.get(hookWord);// cluster is {target,[array of patterns]}    
+					//for each hook , cluster patternt by target
+					patternsAndTargets = splittedLine[1];
+					splittedPatternsTargets = patternsAndTargets.split("\\|");
+					for (String pair : splittedPatternsTargets) {
+						currPatternAndTarget = pair.split("##");
+						pattern = currPatternAndTarget[0];
+						target = currPatternAndTarget[1];
+						if (currHookHashMap.containsKey(target)) {
+							if (!currHookHashMap.get(target).contains(pattern))
+								currHookHashMap.get(target).add(pattern);
+						}
+						else {
+							currHookHashMap.put(target, new ArrayList<String>());  // new cluster
 							currHookHashMap.get(target).add(pattern);
+						}
+					}
+
 				}
-				else {
-					currHookHashMap.put(target, new ArrayList<String>());  // new cluster
-					currHookHashMap.get(target).add(pattern);
-				}
+				br.close();
+			}
+			BufferedWriter out = new BufferedWriter(new FileWriter("output/step5/outbefore.txt"));
+			out.write(gson.toJson(hooksAndClusters));
+			out.close();
+
+			//merge per hook worde 2/3 common		 ? check for fix point	
+			// Second
+			//hook                //targets   //patterns
+			for(Entry<String, HashMap<String, List<String>>> hookANDtargetPattens : hooksAndClusters.entrySet()) {
+				Iterator<Entry<String, List<String>>> innerIterator = hookANDtargetPattens.getValue().entrySet().iterator();
+				//now i have iterator for all target and patterns for specific hook 
+				while (innerIterator.hasNext()) {
+					Map.Entry<String, List<String>> curr = innerIterator.next(); //target and list of patterns
+					while (innerIterator.hasNext()) {
+						Map.Entry<String, List<String>> next = innerIterator.next();
+						if (shouldmerge(curr.getValue(), next.getValue())) { // if true merge and delete smaller
+							curr.getValue().removeAll(next.getValue());
+							curr.getValue().addAll(next.getValue());
+							hookANDtargetPattens.getValue().remove(next.getKey());
+							innerIterator = hookANDtargetPattens.getValue().entrySet().iterator();
+							break;
+						}      ///changing something while iterate over it , maybe not good
+					}
+				}  
+
 			}
 
-		}
-		br.close();
-				BufferedWriter out = new BufferedWriter(new FileWriter("output/step5/outbefore.txt"));
-				out.write(gson.toJson(hooksAndClusters));
-				out.close();
-				
-	//merge per hook worde 2/3 common		 ? check for fix point	
-		// Second
-		//hook                //targets   //patterns
-		for(Entry<String, HashMap<String, List<String>>> hookANDtargetPattens : hooksAndClusters.entrySet()) {
-			Iterator<Entry<String, List<String>>> innerIterator = hookANDtargetPattens.getValue().entrySet().iterator();
-			//now i have iterator for all target and patterns for specific hook 
-			while (innerIterator.hasNext()) {
-				Map.Entry<String, List<String>> curr = innerIterator.next(); //target and list of patterns
-				while (innerIterator.hasNext()) {
-					Map.Entry<String, List<String>> next = innerIterator.next();
-					if (shouldmerge(curr.getValue(), next.getValue())) { // if true merge and delete smaller
-						curr.getValue().removeAll(next.getValue());
-						curr.getValue().addAll(next.getValue());
-						hookANDtargetPattens.getValue().remove(next.getKey());
-						innerIterator = hookANDtargetPattens.getValue().entrySet().iterator();
-						break;
-					}      ///changing something while iterate over it , maybe not good
-				}
-			}  
-
-		}
-
-		BufferedWriter out2 = new BufferedWriter(new FileWriter("output/step5/outafter.txt"));
-		Type type = new TypeToken<HashMap<String,HashMap<String, List<String>>>>(){}.getType();
-		out2.write(gson.toJson(hooksAndClusters,type));
-		out2.close();
+			BufferedWriter out2 = new BufferedWriter(new FileWriter("output/step5/outafter.txt"));
+			Type type = new TypeToken<HashMap<String,HashMap<String, List<String>>>>(){}.getType();
+			out2.write(gson.toJson(hooksAndClusters,type));
+			out2.close();
 	}
 
 	private static boolean shouldmerge(List<String> curr, List<String> next) {
--- step6_34_123.java
+++ step6_34_123.java
@@ -1,42 +1,27 @@
+import java.awt.Toolkit;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-import com.amazonaws.services.opsworks.model.App;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.apache.hadoop.io.LongWritable;
-
 
 //remove pattern that appers in only 1 curpuse
+
+
 public class step6_34_123 {
 	private static final float S = (float) 2/3;
 
@@ -54,104 +39,122 @@ public class step6_34_123 {
 		Type type = new TypeToken<HashMap<String,HashMap<String, List<String>>>>(){}.getType();
 		hooksAndClusters = gson.fromJson(br, type);
 
-		FileInputStream fstream2 = new FileInputStream(args[1]);
-		BufferedReader br2 = new BufferedReader(new InputStreamReader(fstream2));
-
-		while ((strLine = br2.readLine()) != null) {    //Remove all patterns originating from a single hook corpus only
-			if (strLine.length() < 2)
-				continue;
-			splittedLine = strLine.split("\\t");
-			pattern = splittedLine[0];
-			splittedtargetbyHook = splittedLine[1].split("\\|");
-			if (splittedtargetbyHook.length > 1)
-				continue;
-			String singlehookword = splittedtargetbyHook[0];
-			//cluster is a target and a set of patterns, for each hook we have several clusters
-			Iterator<Entry<String, Object>> innerIterator = hooksAndClusters.get(singlehookword).entrySet().iterator();
-			//now i have iterator for target and patterns for specific hook 
-			while (innerIterator.hasNext()) {
-				((List<String>)innerIterator.next().getValue()).remove(pattern);
-			}
-		}
+		/////////////////////////////////////////////////
 
 
+		FilenameFilter filter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("byPattern");}};
 
-		///mark all patterns as unconfirmed
-		for(Entry<String, HashMap<String, Object>> hookANDtargetPatterns : hooksAndClusters.entrySet()) {
-			Iterator<Entry<String, Object>> innerIterator = hookANDtargetPatterns.getValue().entrySet().iterator();
-			while (innerIterator.hasNext()) {
-				Map.Entry<String, Object> curr = innerIterator.next();
-				List<String> currentListOfPatterns = (List) curr.getValue();
-				List<Pair<String,Integer>> newListOfPatterns = new ArrayList<Pair<String,Integer>>();
-				for (String currPattern : currentListOfPatterns) {
-					newListOfPatterns.add(new Pair<String, Integer>(currPattern, 0));
+				File folder = new File(args[1]);
+				File[] listOfFiles = folder.listFiles(filter);
+				for (int i = 0; i < listOfFiles.length; i++) {
+					File file = listOfFiles[i];
+					FileInputStream fstream2 = new FileInputStream(file);
+					BufferedReader br2 = new BufferedReader(new InputStreamReader(fstream2));
+
+					while ((strLine = br2.readLine()) != null) {    //Remove all patterns originating from a single hook corpus only
+						if (strLine.length() < 2)
+							continue;
+						splittedLine = strLine.split("\t");
+						pattern = splittedLine[0];
+						splittedtargetbyHook = splittedLine[1].split("\\|");
+						if (splittedtargetbyHook.length > 1)
+							continue;
+						String singlehookword = splittedtargetbyHook[0];
+						//cluster is a target and a set of patterns, for each hook we have several clusters
+						Iterator<Entry<String, Object>> innerIterator = hooksAndClusters.get(singlehookword).entrySet().iterator();
+						//now i have iterator for target and patterns for specific hook 
+						while (innerIterator.hasNext()) {
+							((List<String>)innerIterator.next().getValue()).remove(pattern);
+						}
+					}
+					br2.close();
 				}
-				curr.setValue(newListOfPatterns);
-
-			}
 
 
-		}
+				///mark all patterns as unconfirmed
+				for(Entry<String, HashMap<String, Object>> hookANDtargetPatterns : hooksAndClusters.entrySet()) {
+					Iterator<Entry<String, Object>> innerIterator = hookANDtargetPatterns.getValue().entrySet().iterator();
+					while (innerIterator.hasNext()) {
+						Map.Entry<String, Object> curr = innerIterator.next();
+						List<String> currentListOfPatterns = (List) curr.getValue();
+						List<Pair<String,Integer>> newListOfPatterns = new ArrayList<Pair<String,Integer>>();
+						for (String currPattern : currentListOfPatterns) {
+							newListOfPatterns.add(new Pair<String, Integer>(currPattern, 0));
+						}
+						curr.setValue(newListOfPatterns);
 
-
-
-		//Type type2 = new TypeToken<HashMap<String,HashMap<String, Pair<String,Integer>>>>(){}.getType();
-		//		BufferedWriter out2 = new BufferedWriter(new FileWriter("output/step341/outbefore342.txt"));
-		//		out2.write(gson.toJson(hooksAndClusters));
-		//		out2.close();
-
-		//3.4.3
-		//target   //pattern //unconfirmed/core
-		System.out.println("Searching for minimal unconfirmed");
-		Pair<String,Entry<String,List<Pair<String,Integer>>>> minimalUnconfirmedCluster;
-		
-		while ((minimalUnconfirmedCluster = getMinimalUnconfirmedCluster(hooksAndClusters)) != null) {
-			//System.out.println("Got a minimal unconfirmed");
-			for(Entry<String, HashMap<String, Object>> cuurHookANDtargetPatterns : hooksAndClusters.entrySet()) {
-				if (cuurHookANDtargetPatterns.getKey().equals(minimalUnconfirmedCluster.getFirst())) //minimalUnconfirmedCluster.first is the hook that the cluster extracted from
-					continue; //(from different hook)
-				
-				Iterator<Entry<String, Object>> innerIterator = cuurHookANDtargetPatterns.getValue().entrySet().iterator();
-				while (innerIterator.hasNext()) {
-					Map.Entry<String, Object> otherCluster = innerIterator.next(); //target and list of patterns
-					if(shouldmerge(minimalUnconfirmedCluster.getSecond().getValue(), (List<Pair<String,Integer>>)otherCluster.getValue())) {
-						System.out.println("Decided to merge!!");
-						System.out.println("hookword1: "+minimalUnconfirmedCluster.getFirst() + "  hookword2: "+cuurHookANDtargetPatterns.getKey());
-						System.out.println("targetword1: "+minimalUnconfirmedCluster.getSecond().getKey()+" t argetword2: "+otherCluster.getKey());
-						
-						mergeListPatternClusters(minimalUnconfirmedCluster.getSecond().getValue(), (List<Pair<String,Integer>>)otherCluster.getValue());
-						cuurHookANDtargetPatterns.getValue().remove(otherCluster.getKey());
-						innerIterator = cuurHookANDtargetPatterns.getValue().entrySet().iterator();
-						//break; //???? if merge starit from beginig??????????????
 					}
 
+
 				}
-			}
-			if (isStillUnconfirmed(minimalUnconfirmedCluster)) { // step 4 in the algorithm : is c1 still unconfirmed? if yes, delete it
-				String hookWord = minimalUnconfirmedCluster.getFirst();
-				String targetWord = minimalUnconfirmedCluster.getSecond().getKey(); // the name of the cluster to delete
-				hooksAndClusters.get(hookWord).remove(targetWord);
-				System.out.println("deleting : "+ targetWord);
-			}
-		}
 
 
-		printoutputcluster(hooksAndClusters , gson);
+
+				//////Type type2 = new TypeToken<HashMap<String,HashMap<String, Pair<String,Integer>>>>(){}.getType(); //no need
+				//					BufferedWriter out2 = new BufferedWriter(new FileWriter("output/step6_34_123/outbefore342.txt"));
+				//					out2.write(gson.toJson(hooksAndClusters));
+				//					out2.close();
+
+				///////////////////////////////////////////////////////////////////////////				
+				System.out.println("start 3.4 big loop");
+				//3.4.3
+				//target   //pattern //unconfirmed/core
+				System.out.println("Searching for minimal unconfirmed");
+				Pair<String,Entry<String,List<Pair<String,Integer>>>> minimalUnconfirmedCluster;
+
+				while ((minimalUnconfirmedCluster = getMinimalUnconfirmedCluster(hooksAndClusters)) != null) {
+					//System.out.println("Got a minimal unconfirmed");
+					for(Entry<String, HashMap<String, Object>> cuurHookANDtargetPatterns : hooksAndClusters.entrySet()) {
+						if (cuurHookANDtargetPatterns.getKey().equals(minimalUnconfirmedCluster.getFirst())) //minimalUnconfirmedCluster.first is the hook that the cluster extracted from
+							continue; //(from different hook)
+
+						Iterator<Entry<String, Object>> innerIterator = cuurHookANDtargetPatterns.getValue().entrySet().iterator();
+						while (innerIterator.hasNext()) {
+							Map.Entry<String, Object> otherCluster = innerIterator.next(); //target and list of patterns
+							if(shouldmerge(minimalUnconfirmedCluster.getSecond().getValue(), (List<Pair<String,Integer>>)otherCluster.getValue())) {
+								//	System.out.println("Decided to merge!!");
+								//	System.out.println("hookword1: "+minimalUnconfirmedCluster.getFirst() + "  hookword2: "+cuurHookANDtargetPatterns.getKey());
+								//	System.out.println("targetword1: "+minimalUnconfirmedCluster.getSecond().getKey()+" t argetword2: "+otherCluster.getKey());
+
+								mergeListPatternClusters(minimalUnconfirmedCluster.getSecond().getValue(), (List<Pair<String,Integer>>)otherCluster.getValue());
+								cuurHookANDtargetPatterns.getValue().remove(otherCluster.getKey());
+								innerIterator = cuurHookANDtargetPatterns.getValue().entrySet().iterator();
+								//break; //???? if merge starit from beginig??????????????
+							}
+
+						}
+					}
+					if (isStillUnconfirmed(minimalUnconfirmedCluster)) { // step 4 in the algorithm : is c1 still unconfirmed? if yes, delete it
+						String hookWord = minimalUnconfirmedCluster.getFirst();
+						String targetWord = minimalUnconfirmedCluster.getSecond().getKey(); // the name of the cluster to delete
+						hooksAndClusters.get(hookWord).remove(targetWord);
+						//System.out.println("deleting : "+ targetWord);
+					}
+				}
 
 
+				printoutputcluster(hooksAndClusters , gson);
+
+				//beep sounds when finish
+				for(int i=0; i<50;i++) {
+					Toolkit.getDefaultToolkit().beep();
+					Thread.sleep(1000);
+				}
 	}
 
 
 
 	//print list of clusters, for all cluster there are 2 list , such that first place is list of all confirmed , second list is the unconfirmd
 	private static void printoutputcluster(HashMap<String, HashMap<String, Object>> hooksAndClusters, Gson gson) throws IOException {
-		BufferedWriter out22 = new BufferedWriter(new FileWriter("output/step341/finish.txt"));
-		
+		BufferedWriter out22 = new BufferedWriter(new FileWriter("output/step6_34_123/finish.txt"));
+
 		List<List<String>> cluster = new ArrayList<List<String>>();
 		List<String> confiremd = new ArrayList<String>();
 		List<String> unconfiremd = new ArrayList<String>();
 		cluster.add(confiremd); cluster.add(unconfiremd);
-		
+
 		Pair<String, Integer> currPattern;
 		for(Entry<String, HashMap<String, Object>> currhookANDtargetPatterns : hooksAndClusters.entrySet()) { //hook
 			Iterator<Entry<String, Object>> innerIterator = currhookANDtargetPatterns.getValue().entrySet().iterator();
